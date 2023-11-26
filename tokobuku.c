9e16cbd0167e7f15765fa028ee7cd161c948c506
Hazel368 <hazelmpr368@gmail.com>
2023-11-26 23:12:54 +0700

    progress 1

--- tokobuku.c
+++ tokobuku.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // Include <string.h> for string-related functions
 
-int main (){
-    printf("Hello World");
+struct tokobuku {
+    char judul[50];   
+    char judul[50];   
+    char penulis[50]; 
+    int tahun;
+    double harga;
+};
+
+int main() {
+    struct tokobuku buku1 = {"Negeri 5 Menara", "Ahmad Fuadi", 2009, 29000};
+
+    printf("Buku 1\n");
+    printf("Judul buku  : %s\n", buku1.judul); // Use %s for strings
+    printf("Penulis buku adalah : %s\n", buku1.penulis);
+    printf("Tahun terbit buku : %d\n", buku1.tahun);
+    printf("Harga buku tersebut : %.2f\n", buku1.harga);
+
+    return 0;
 }
